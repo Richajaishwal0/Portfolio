2441bfb104b3462f238203771b38b2195de82d80
richa <2023richajaishwal@gmail.com>
2024-04-19 20:29:20 +0530

    my one-one commit

--- script.js
+++ script.js
@@ -28,12 +28,3 @@ let sections =document.querySelectorAll('section');
           }
         });
   
-        document.getElementById('do').addEventListener('click', function() {
-          var content = document.getElementById('end');
-          if (content.style.display === 'none') {
-            content.style.display = 'block';
-           
-           } else {
-            content.style.display = 'none';
-          }
-        });
